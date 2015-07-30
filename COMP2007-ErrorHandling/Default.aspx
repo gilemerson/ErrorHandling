0d72c949bbc572d830b6dc9d5b992b36c5001311
Gil Emerson <emerson-gil@outlook.com>
2015-07-30 12:09:35 -0400

    Error Handling COMP2007

---
+++ COMP2007-ErrorHandling/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Welcome" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="COMP2007_ErrorHandling.Default1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+    <div class="jumbotron text-center">
+        <h1>Welcome Home</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div> 
+  </div>
+</asp:Content>
---
+++ COMP2007-ErrorHandling/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace COMP2007_ErrorHandling
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                // Pass the error on to the error page.
+                Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax", true);
+            }
+
+        }
+    }
+}
